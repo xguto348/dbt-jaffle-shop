1a6fa5bf57adf8e46f870dcaddbbd203487de258
luiz-azevedo-analytics <azluizg@gmail.com>
2026-03-21 23:01:07 +0000

    generated models from clicking "generate model" on yaml file

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,7 +1,22 @@
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+with 
 
-    from {{ source('jaffle_shop', 'orders') }}
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        user_id,
+        order_date,
+        status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
